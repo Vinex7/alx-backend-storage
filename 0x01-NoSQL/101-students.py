155efce0e6b494fafb69b197fb121d43065946e7
Vinex7 <thabelosilima@gmail.com>
2024-08-12 15:14:18 -0700

    NoSQL

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,36 +1,28 @@
 #!/usr/bin/env python3
-"""
-Function to return all students sorted by average score
-"""
+'''Task 14's module.
+'''
 
-from pymongo import MongoClient
 
 def top_students(mongo_collection):
-    """
-    Returns all students sorted by average score.
-    
-    Parameters:
-    - mongo_collection: the pymongo collection object
-
-    Returns:
-    - List of students sorted by average score with key = averageScore
-    """
-    pipeline = [
-        {
-            "$project": {
-                "name": 1,
-                "averageScore": {"$avg": "$scores.score"}
-            }
-        },
-        {
-            "$sort": {"averageScore": -1}
-        }
-    ]
-    return list(mongo_collection.aggregate(pipeline))
-
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.school
-    collection = db.students
-    for student in top_students(collection):
-        print(student)
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
